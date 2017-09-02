3eaed6e8e1e2d6e9dc419b47784db28a392c419a
ArthurPai <baidragoon@gmail.com>
2017-09-02 15:01:28 +0800

    enter bulletin, and back to lobby

---
+++ Assets/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public UIPanel m_LobbyPanel;
+    public UIPanel m_BulletinPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+  
+    public void OpenBulletin() {
+        m_LobbyPanel.gameObject.SetActive(false);
+        m_BulletinPanel.gameObject.SetActive(true);
+    }
+
+    public void BackLobby() {
+        m_LobbyPanel.gameObject.SetActive(true);
+        m_BulletinPanel.gameObject.SetActive(false);
+    }
+}
