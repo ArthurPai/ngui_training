b251a0c1e688c38e65f060c3993942c10fb94933
ArthurPai <baidragoon@gmail.com>
2017-09-02 17:34:10 +0800

    add message to message board

---
+++ Assets/BulletinManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletinManager : MonoBehaviour
+{
+    public GameObject MessagePrefab;
+
+    public GameObject MessageGrid;
+    public GameObject MessageInput;
+
+    int m_MessageCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SendNewMessage() {
+        string message = MessageInput.GetComponent<UILabel>().text;
+
+        if (message.Length > 0) {
+            AddMessage(message);
+            MessageInput.GetComponent<UILabel>().text = "";
+        }
+    }
+
+    public void AddMessage(string message) {
+        GameObject item = NGUITools.AddChild(MessageGrid, MessagePrefab);
+
+        m_MessageCount++;
+        item.name = "message" + m_MessageCount;
+        item.GetComponent<UILabel>().text = message;
+
+        MessageGrid.GetComponent<UIGrid>().Reposition();
+    }
+}
